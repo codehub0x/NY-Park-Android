359138414a0d621088f307eccae2c062ffc9fc98
Kane W <kanew071@gmail.com>
2019-11-28 00:15:18 +0800

    Implement the image picker logic

--- app/src/main/java/redhat/org/ipark/AccountActivity.java
+++ app/src/main/java/redhat/org/ipark/AccountActivity.java
@@ -1,19 +1,39 @@
 package redhat.org.ipark;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.io.IOException;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,6 +42,9 @@ import butterknife.OnClick;
 import butterknife.OnTouch;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private static final String TAG = AccountActivity.class.getSimpleName();
+    public static final int REQUEST_IMAGE = 100;
 
     @BindView(R.id.account_image_avatar)
     RoundedImageView imageAvatar;
@@ -40,34 +63,6 @@ public class AccountActivity extends AppCompatActivity {
     @BindView(R.id.account_switch_notification)
     Switch switchNotification;
 
-    @OnClick(R.id.account_btn_save)
-    public void onClickSave(View view) {
-
-    }
-
-    @OnClick(R.id.account_layout_payment)
-    public void onClickPayment(View view) {
-
-    }
-
-    @OnClick(R.id.account_image_avatar)
-    public void onClickAvatar(View view) {
-
-    }
-
-    @OnCheckedChanged(R.id.account_switch_notification)
-    public void onChangeNotification(Switch switchNotif, boolean state) {
-
-    }
-
-    @OnTouch(R.id.account_scrollView)
-    public boolean onTouchScrollView(View view, MotionEvent event) {
-        if (event != null && event.getAction() == MotionEvent.ACTION_MOVE) {
-            hideKeyboard(view);
-        }
-        return false;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,7 +71,13 @@ public class AccountActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
 
-        initialize();
+        btnSave.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.colorYellow));
+
+        loadProfileDefault();
+        // Clearing older images from cache directory
+        // don't call this line if you want to choose multiple images in the same activity
+        // call this once the bitmap(s) usage is over
+        ImagePickerActivity.clearCache(this);
     }
 
     @Override
@@ -92,16 +93,169 @@ public class AccountActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.nothing, R.anim.left_to_right);
     }
 
-    private void initialize() {
-        btnSave.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.colorYellow));
-    }
-
     private void hideKeyboard(View view) {
         InputMethodManager imm = ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE));
         boolean isKeyboardUp = imm.isAcceptingText();
 
         if (isKeyboardUp) {
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void loadProfile(String url) {
+        Log.d(TAG, "Image cache path: " + url);
+
+        Glide.with(this).load(url)
+                .into(imageAvatar);
+        imageAvatar.setColorFilter(ContextCompat.getColor(this, android.R.color.transparent));
+    }
+
+    private void loadProfileDefault() {
+        Glide.with(this).load(R.drawable.avatar)
+                .into(imageAvatar);
+        imageAvatar.setColorFilter(ContextCompat.getColor(this, android.R.color.transparent));
+    }
+
+    @OnClick(R.id.account_btn_save)
+    void onClickSave(View view) {
+
+    }
+
+    @OnClick(R.id.account_layout_payment)
+    void onClickPayment(View view) {
+
+    }
+
+    @OnCheckedChanged(R.id.account_switch_notification)
+    void onChangeNotification(Switch switchNotif, boolean state) {
+
+    }
+
+    @OnTouch(R.id.account_scrollView)
+    boolean onTouchScrollView(View view, MotionEvent event) {
+        if (event != null && event.getAction() == MotionEvent.ACTION_MOVE) {
+            hideKeyboard(view);
+        }
+        return false;
+    }
+
+    @OnClick(R.id.account_image_avatar)
+    void onClickAvatar(View view) {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            showImagePickerOptions();
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void showImagePickerOptions() {
+        ImagePickerActivity.showImagePickerOptions(this, getSupportFragmentManager(), new ImagePickerActivity.PickerOptionListener() {
+            @Override
+            public void onTakeCameraSelected() {
+                launchCameraIntent();
+            }
+
+            @Override
+            public void onChooseGallerySelected() {
+                launchGalleryIntent();
+            }
+        });
+    }
+
+    private void launchCameraIntent() {
+        Intent intent = new Intent(AccountActivity.this, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_IMAGE_CAPTURE);
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickerActivity.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_WIDTH, 200);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_HEIGHT, 200);
+
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    private void launchGalleryIntent() {
+        Intent intent = new Intent(AccountActivity.this, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_GALLERY_IMAGE);
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickerActivity.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_WIDTH, 200);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_HEIGHT, 200);
+
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    /**
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     * NOTE: Keep proper title and message depending on your app
+     */
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AccountActivity.this);
+        builder.setTitle(getString(R.string.dialog_permission_title));
+        builder.setMessage(getString(R.string.dialog_permission_message));
+        builder.setPositiveButton(getString(R.string.go_to_settings), (dialog, which) -> {
+            dialog.cancel();
+            openSettings();
+        });
+        builder.setNegativeButton(getString(android.R.string.cancel), (dialog, which) -> dialog.cancel());
+        builder.show();
+
+    }
+
+    // navigating user to app settings
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getParcelableExtra("path");
+                try {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    } else {
+                        ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), uri);
+                        Bitmap bitmap = ImageDecoder.decodeBitmap(source);
+                    }
+
+                    // loading profile image from local cache
+                    loadProfile(uri.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
